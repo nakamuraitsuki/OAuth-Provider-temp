ec9b86299a354bea80f4d59639409c8439f13802
nakamuraitsuki <ituki1911n@gmail.com>
2026-03-01 21:13:01 +0900

    feat: interfaceにその他必要なものを実装 reouter APIルーティングの初期化関数を定義 renderer SSRのためのテンプレートの読み出し

---
+++ internal/interface/http/router.go
@@ -0,0 +1,27 @@
+package http
+
+import (
+	"net/http"
+
+	"example.com/m/internal/interface/http/auth"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+)
+
+func InitRoutes(
+	e *echo.Echo,
+	authHandler *auth.AuthHandler,
+) {
+	e.GET("/login", authHandler.ShowLogin)
+	e.POST("/login", authHandler.Login)
+	e.GET("/dashboard", authHandler.Dashboard)
+
+	// ログイン状態によるリダイレクト分岐
+	e.GET("/", func(c echo.Context) error {
+		sess, _ := session.Get("session", c)
+		if sess.Values["user_id"] != nil {
+			return c.Redirect(http.StatusSeeOther, "/dashboard")
+		}
+		return c.Redirect(http.StatusSeeOther, "/login")
+	})
+}
