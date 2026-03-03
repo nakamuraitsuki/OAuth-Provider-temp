f97d40a91c598eb82e29d37ece17da3e433da6a7
nakamuraitsuki <ituki1911n@gmail.com>
2026-03-04 01:02:13 +0900

    feat: openid スコープの追加

--- internal/domain/oauth/value/scope.go
+++ internal/domain/oauth/value/scope.go
@@ -2,4 +2,7 @@ package value
 
 const (
 	ScopeProfileRead = "profile:read"
+	// [OpenID Core 1.0 section 3.1.2.1] Authentication Request
+	// で定義されている "openid" スコープも追加しておく
+	ScopeOpenID      = "openid"
 )
--- internal/infrastructure/persistence/postgres/oauth/scope.go
+++ internal/infrastructure/persistence/postgres/oauth/scope.go
@@ -7,9 +7,11 @@ import "example.com/m/internal/domain/oauth/value"
 
 var ScopesTable = map[int]string{
 	1: value.ScopeProfileRead,
+	2: value.ScopeOpenID,
 }
 
 // 逆引き用
 var ScopeIDs = map[string]int{
 	value.ScopeProfileRead: 1,
+	value.ScopeOpenID:      2,
 }
