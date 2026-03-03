a1544d4401c57b3cb3479227be7e2d31d7524bd2
nakamuraitsuki <ituki1911n@gmail.com>
2026-03-03 20:20:52 +0900

    feat: scope を先に定義

---
+++ internal/infrastructure/persistence/postgres/oauth/scope.go
@@ -0,0 +1,15 @@
+package oauth
+
+import "example.com/m/internal/domain/oauth/value"
+
+// Scope の定義とテーブルはメモリに逃して簡易実装。
+// どうせScopeは動的にいじったりしないので
+
+var ScopesTable = map[int]string{
+	1: value.ScopeProfileRead,
+}
+
+// 逆引き用
+var ScopeIDs = map[string]int{
+	value.ScopeProfileRead: 1,
+}
