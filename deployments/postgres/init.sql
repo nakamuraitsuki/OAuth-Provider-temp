944a18ff939d0df0cfd64ad420a651a2538d4de1
nakamuraitsuki <ituki1911n@gmail.com>
2026-03-04 13:52:46 +0900

    feat: db 初期化を定義

--- deployments/postgres/init.sql
+++ deployments/postgres/init.sql
@@ -12,3 +12,45 @@ CREATE TABLE IF NOT EXISTS credentials (
     password_hash TEXT NOT NULL,
     updated_at    TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS authorization_codes (
+    code            VARCHAR(255) PRIMARY KEY,
+    client_id       UUID NOT NULL,
+    user_id         UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    redirect_uri    TEXT NOT NULL,
+    state           TEXT,
+    nonce           TEXT,
+    expires_at      TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS authorization_code_scopes (
+    code VARCHAR(255) REFERENCES authorization_codes(code) ON DELETE CASCADE,
+    scope_id INTEGER NOT NULL,
+    PRIMARY KEY (code, scope_id)
+);
+
+CREATE TABLE IF NOT EXISTS access_tokens (
+    token VARCHAR(255) PRIMARY KEY,
+    client_id UUID NOT NULL,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    expires_at TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS access_token_scopes (
+    token VARCHAR(255) REFERENCES access_tokens(token) ON DELETE CASCADE,
+    scope_id INTEGER NOT NULL,
+    PRIMARY KEY (token, scope_id)
+);
+
+CREATE TABLE IF NOT EXISTS refresh_tokens (
+    token VARCHAR(255) PRIMARY KEY,
+    client_id UUID NOT NULL,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    expires_at TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS refresh_token_scopes (
+    token VARCHAR(255) REFERENCES refresh_tokens(token) ON DELETE CASCADE,
+    scope_id INTEGER NOT NULL,
+    PRIMARY KEY (token, scope_id)
+);
